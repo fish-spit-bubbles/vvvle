abfdf918a21ab61c42725101cc45db07fcc43e46
TaiChiPandaBoxing <809457175@qq.com>
2017-08-03 10:39:17 +0800

    第二次修该sidebar

--- Index/Common/js/sidebar.js
+++ Index/Common/js/sidebar.js
@@ -9,13 +9,13 @@ $(".sidebar_nav li").on('mouseout', function() {
     $(".sidebar_nav span").hide().css("right", "50px");
 });
 // 登录
-var login = '<div class="from_warp"><h4>用户登录</h4><a href="javascript:;" class="panl_close"></a><div><label for="sbarUsername">账户：</label><input id="sbarUsername" type="text" name="username" value=""/></div><div><label for="sbarUserpwd">密码：</label><input id="sbarUserpwd" type="password" name="userpwd" value=""/></div><div class="sbar_btn"><button type="submit"></button><span>找回密码</span></div><div class="logingo"><a href="#">新用户注册</a></div><div class="login_info">您还可以使用合作网站账号登录：</div><div class="sbar_icon"><a href="#"><img src="./Index/Common/img/qq.png" alt="qq"></a><a href="#"><img src="./Index/Common/img/sina.png" alt="sina"></a><a href="#"><img src="./Index/Common/img/alipay.png" alt="alipay"></</div></div>';
+var login = '<div class="from_warp"><h4>用户登录</h4><a href="javascript:;" class="panl_close"></a><div><label for="sbarUsername">账户：</label><input id="sbarUsername" type="text" name="username" value=""/></div><div><label for="sbarUserpwd">密码：</label><input id="sbarUserpwd" type="password" name="userpwd" value=""/></div><div class="sbar_btn"><button type="submit"></button><span>找回密码</span></div><div class="logingo"><a href="#">新用户注册</a></div><div class="login_info">您还可以使用合作网站账号登录：</div><div class="sbar_icon"><a href="#"></a><a href="#"></a><a href="#"></</div></div>';
 
 // 购物车
 var cart = '<div class="cart_empty"></div>';
 
 // var historys = '111';
-var historys = '<div class="history_list"><div><h4>我看过的</h4><a href="javascript:;" class="panl_close"></a></div><a href="#"><img src="./Index/Common/img/1125_thumb_G_1398648339553.png" alt="img" /><span>OLIVI 摩芙巧克力 <i>&yen;</i><b>241</b></span></a><a href="#"><img src="./Index/Common/img/1125_thumb_G_1398648339553.png" alt="img" /><span>OLIVI 摩芙巧克力 <i>&yen;</i><b>241</b></span></a><a href="#"><img src="./Index/Common/img/1125_thumb_G_1398648339553.png" alt="img" /><span>OLIVI 摩芙巧克力 <i>&yen;</i><b>241</b></span></a><p>推荐商品</p><dl><dt><a href="#"><img src="./Index/Common/img/1125_thumb_G_1398648339553.png" alt="" /></a></dt><dd><a href="#"><span>OLIVI 摩芙巧克力</span></a></dd><dd><i>&yen;</i><b>241</b></dd></dl><dl><dt><a href="#"><img src="./Index/Common/img/1125_thumb_G_1398648339553.png" alt="" /></a></dt><dd><a href="#"><span>OLIVI 摩芙巧克力</span></a></dd><dd><i>&yen;</i><b>241</b></dd></dl><a class="sidebar_more" href="#">更多...</a></div>'
+var historys = '<div class="history_list"><div><h4>我看过的</h4><a href="javascript:;" class="panl_close"></a></div><a href="#"><span class="img"></span><span>OLIVI 摩芙巧克力 <i>&yen;</i><b>241</b></span></a><a href="#"><span class="img"></span><span>OLIVI 摩芙巧克力 <i>&yen;</i><b>241</b></span></a><a href="#"><span class="img"></span><span>OLIVI 摩芙巧克力 <i>&yen;</i><b>241</b></span></a><p>推荐商品</p><dl><dt><a href="#"><span class="img"></span></a></dt><dd><a href="#"><span>OLIVI 摩芙巧克力</span></a></dd><dd><i>&yen;</i><b>241</b></dd></dl><dl><dt><a href="#"><span class="img"></span></a></dt><dd><a href="#"><span>OLIVI 摩芙巧克力</span></a></dd><dd><i>&yen;</i><b>241</b></dd></dl><a class="sidebar_more" href="#">更多...</a></div>'
 // 上一个下标
 var lastIndex = -1;
 $(".sidebar_nav li").on('click', function() {
@@ -70,6 +70,9 @@ $(".sidebar_nav li").on('click', function() {
                         $(".sidebar_show").hide();
                     });
                 }).html(historys);
+            } else {
+                $(".sidebar_nav li").removeClass("li_click");
+                $(".sidebar_nav li").removeClass("li_hover");
             }
             lastIndex = $(this).index();
         }   
@@ -85,15 +88,17 @@ $(".sidebar_nav .sbar_close").click(function() {
         $('.sidebar_warp').animate({
             right: -35
         }, 100);
-        $('.sidebar_tool').animate({
+        $('.sidebar_tool').show().animate({
             right: 35
         }, 100);
     });
 });
 $(".sidebar_tool").click(function(){
-    $('.sidebar_tool').animate({
+    $('.sidebar_tool').show().animate({
         right: -40
-    }, 100);
+    }, 100, function(){
+        $('.sidebar_tool').hide();
+    });
     $('.sidebar_warp').animate({
         right: 0
     }, 100);
