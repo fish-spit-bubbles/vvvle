81efbc30e90ea3f67378c703c5e8b8b23b4c417d
1040964968@qq.com <lanou@fengyang-Edward-Feng.local>
2017-08-03 10:42:16 +0800

    第二次修改

--- Index/Runtime/Cache/b81156bce0041080cc4f324db61247d2.php
+++ Index/Runtime/Cache/b81156bce0041080cc4f324db61247d2.php
@@ -9,17 +9,17 @@
     <link rel="stylesheet" type="text/css" href="__ROOT__/Index/Common/libs/bootstrap/css/bootstrap.min.css" />
     <link rel="stylesheet" type="text/css" href="__ROOT__/Index/Common/css/sidebar.css" />
     <link rel="stylesheet" type="text/css" href="__ROOT__/Index/Common/css/footer.css" />
+    <link rel="stylesheet" type="text/css" href="__ROOT__/Index/Common/css/slideshow.css" />
     <link rel="stylesheet" type="text/css" href="__ROOT__/Index/Common/css/lens.css" />
-
 </head>
 <body>
    <div class="lens_wrap">
    <!-- 轮播图 -->
        <div class="lunbo" id="lunbo">
     <ul id="lunboUl" class="lunboUl">
-        <li><a href="##"><img src="__ROOT__/Index/Common/img_lens/lens_imgs01.jpg" alt="" /></a></li>
-        <li><a href="##"><img src="__ROOT__/Index/Common/img_lens/lens_imgs02.jpg" alt="" /></a></li>
-        <li><a href="##"><img src="__ROOT__/Index/Common/img_lens/lens_imgs01.jpg" alt="" /></a></li>
+        <li><a href="##"><img src="__ROOT__/Index/Common/img/img_lens/lens_imgs01.jpg" alt="" /></a></li>
+        <li><a href="##"><img src="__ROOT__/Index/Common/img/img_lens/lens_imgs02.jpg" alt="" /></a></li>
+        <li><a href="##"><img src="__ROOT__/Index/Common/img/img_lens/lens_imgs01.jpg" alt="" /></a></li>
     </ul>	
         <ol id="lunboOl">
             <li class="actBtn"></li>
@@ -30,9 +30,71 @@
 </div>
    <!-- 筛选列表 -->
         <div class="select_box">
-            
+            <div>品牌:
+            	<div></div>
+            	<div></div>
+            </div>
+            <div>直径:</div>
+            <div>颜色:</div>
+            <div>更换周期:</div>
+            <div>效果:</div>
+            <div>价格:</div>
+
         </div>
     </div>
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+
+
+    <div class="sidebar_warp">
+    <div class="sidebar">
+        <ul class="sidebar_nav">
+            <li>
+                <i class="profile_icon"></i>
+                <span>个人中心</span>
+            </li>
+            <li class="sbar_cat">
+                <i class="sbar_cat_icon"></i>
+                <p>购物袋</p>
+                <i class="sbar_nub_icon">0</i>
+            </li>
+            <li>
+                <i class="serivce_icon"></i>
+                <span class="conact_img"><img src="__ROOT__/Index/Common/img/conact_float.png" alt="联系方式"/></span>
+            </li>
+            <li>
+                <i class="favor_icon"></i>
+                <span>我的收藏</span>
+            </li>
+            <li>
+                <i class="history_icon"></i>
+                <span>我看过的</span>
+            </li>
+            <li>
+                <i class="qrcode_icon"></i>
+                <span class="qrcode"><img src="__ROOT__/Index/Common/img/qrcode.png" alt="微信客服"/></span>
+            </li>
+            <li class="sbar_btm sbar_top">
+                <i class="sbar_top_icon"></i>
+                <span>返回顶部</span>
+            </li>
+            <li class="sbar_btm sbar_close">
+                <i class="sbar_close_icon"></i>
+                <span>关闭工具栏</span>
+            </li>
+        </ul>
+        <div class="sidebar_tool">
+            <img src="__ROOT__/Index/Common/img/float_tool.png" alt="tool" />
+        </div>
+    </div>
+    <div class="sidebar_show">
+
+    </div>
+</div>
+=======
+>>>>>>> origin/master
+>>>>>>> Stashed changes
     <div class="header">
         <!--更多-->
 <div class="home_footer">
@@ -183,5 +245,6 @@
 </body>
 <script type="text/javascript" src="__ROOT__/Index/Common/js/jquery.min.js"></script>
 <script type="text/javascript" src="__ROOT__/Index/Common/js/sidebar.js"></script>
+<script type="text/javascript" src="__ROOT__/Index/Common/js/slideshow.js"></script>
 <script type="text/javascript" src="__ROOT__/Index/Common/js/lens.js"></script>
 </html>
