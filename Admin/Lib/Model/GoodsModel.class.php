43f44b079f4c90481a021524967983f364c16b98
TaiChiPandaBoxing <809457175@qq.com>
2017-08-07 10:58:01 +0800

    goods
    goods

--- Admin/Lib/Model/GoodsModel.class.php
+++ Admin/Lib/Model/GoodsModel.class.php
@@ -1,6 +1,15 @@
 <?php
-    class GoodsModel extends Model {
-        public function getGoods(){
-            $goods = M("");
-        }
+class GoodsModel extends Model {
+    public function getGoodsOne($id){
+        $goods = M("goods");
+        $where["id"] = $id; 
+        $result = $goods->where($where)->find();
+        return $result;
+    }
+    public function getGoodsList($name){
+        $goods = M("goods");
+        $where = $name;
+        $result = $goods->where($where)->select();
+        return $result;
+    }
 }
--- Index/Lib/Model/GoodsModel.class.php
+++ Index/Lib/Model/GoodsModel.class.php
@@ -1,8 +1,15 @@
 <?php
-    class GoodsModel extends Model {
-        public function getGoods(){
-            $goods = M("goods");
-            $
-        }
-
+class GoodsModel extends Model {
+    public function getGoodsOne($id){
+        $goods = M("goods");
+        $where["id"] = $id; 
+        $result = $goods->where($where)->find();
+        return $result;
+    }
+    public function getGoodsList($name){
+        $goods = M("goods");
+        $where = $name;
+        $result = $goods->where($where)->select();
+        return $result;
+    }
 }
