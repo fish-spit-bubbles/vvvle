9636fb2b80578f87eb6d57b3c629bd7f1e4ea650
TaiChiPandaBoxing <809457175@qq.com>
2017-08-12 09:45:47 +0800

    search
    search

--- Index/Lib/Action/ProductDetailsAction.class.php
+++ Index/Lib/Action/ProductDetailsAction.class.php
@@ -14,29 +14,22 @@ class ProductDetailsAction extends Action {
         $goods = M("Goods");
         $listArr = $goods->where("id=".$id)->find();
         $this->assign("v",$listArr );
-
-
         // 详情传入 uid pid
         $uid = $_SESSION['uid'];
         $pid = $_GET['id'];
         $this->assign("uid",$uid);
         $this->assign("pid",$pid);
-
         $this->display("productDetails");
     }
     // 获取商品详情，添加成功数据库 跳转到购物车
     public function getGoods(){
-              
-   
         // 判断用户是否有登陆
-
         if(!empty($_SESSION['username'])){
             $str = file_get_contents("php://input");
             $newstr = json_decode($str,true); 
             $adddata = D("ProductDetails");
         // 先判断有数据的时候直接跳转。没有数据添加数据在跳转
             $query = $adddata->query($newstr);
-
             if($query){
             //  跳转   返回状态吗
                     $info['status']=1;
@@ -50,45 +43,52 @@ class ProductDetailsAction extends Action {
                 }else{
                     $info['info']="添加失败";
                     $info['status']=0;
-                };
-                   
+                };        
             };  
-
         }else{
              $info['status']=2;
         }
          $this->ajaxReturn($info); 
              
     }
-
-
     public function getGoodsOne(){          
-            $str = file_get_contents("php://input");
-            $newstr = json_decode($str,true); 
-            $adddata = D("ProductDetails");
-        // 先判断有数据的时候直接跳转。没有数据添加数据在跳转
-            $query = $adddata->query($newstr);
+        $str = file_get_contents("php://input");
+        $newstr = json_decode($str,true); 
+        $adddata = D("ProductDetails");
+    // 先判断有数据的时候直接跳转。没有数据添加数据在跳转
+        $query = $adddata->query($newstr);
 
-            if($query){
-            //  跳转   返回状态吗
-                    $info['status']=1;
-                    $this->ajaxReturn($info); 
+        if($query){
+        //  跳转   返回状态吗
+                $info['status']=1;
+                $this->ajaxReturn($info); 
+        }else{
+            $res = $adddata->adddata($newstr);   
+            if($res){
+                $info['info']="添加成功";
+                $info['status']=1;
+            
             }else{
-                $res = $adddata->adddata($newstr);   
-                if($res){
-                    $info['info']="添加成功";
-                    $info['status']=1;
-                
-                }else{
-                    $info['info']="添加失败";
-                    $info['status']=0;
-                };
-                    $this->ajaxReturn($info); 
-            };  
-           
+                $info['info']="添加失败";
+                $info['status']=0;
+            };
+                $this->ajaxReturn($info); 
+        };        
     }
-
-
-
-
+    public function getDetailGoods(){
+        $txt = $_POST["qing"];
+        $goods = D("Goods");
+        $res = $goods->getGoodsOne($txt);
+        if ($res) {
+            $info['info']="成功";
+            $info['status']=1;
+            $info['data'] = $res;
+            $info['name'] = $txt;
+        } else {
+            $info['info']="失败";
+            $info['status']=0;
+            $info['name'] = $txt;
+        }
+        $this->ajaxReturn($info);
+    }
 }
--- Index/Lib/Model/GoodsModel.class.php
+++ Index/Lib/Model/GoodsModel.class.php
@@ -3,7 +3,13 @@
 class GoodsModel extends Model {
     protected $tableName = "goods";
     public function getGoodsList($name, $start, $number){
-        $result = $this->where("brand='".$name."'")->order("id DESC")->limit($start, $number)->select();
+        $where['productName'] = array('like', "%{$name}%");
+        $result = $this->where($where)->order("id DESC")->limit($start, $number)->select();
+        return $result;
+    }
+    public function getGoodsOne($name){
+        $where['productName'] = array('like', "%{$name}%");
+        $result = $this->where($where)->order("id DESC")->find();
         return $result;
     }
 }
