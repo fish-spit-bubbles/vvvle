1914952e58da693698c64137d3431422f8afad7e
fish-spit-bubbles <1072188607@qq.com>
2019-02-14 14:03:34 +0800

    Update config.php

--- Index/Conf/config.php
+++ Index/Conf/config.php
@@ -2,10 +2,10 @@
 return array(
 	//'配置项'=>'配置值'
 	'DB_TYPE'   => 'mysql', // 数据库类型
-    'DB_HOST'   => 'bdm27488675.my3w.com', // 服务器地址
-    'DB_NAME'   => 'bdm27488675_db', // 数据库名
-    'DB_USER'   => 'bdm27488675', // 用户名
-    'DB_PWD'    => 'SH1302070001', // 密码
+    'DB_HOST'   => '', // 服务器地址
+    'DB_NAME'   => '', // 数据库名
+    'DB_USER'   => '', // 用户名
+    'DB_PWD'    => '', // 密码
     'DB_PORT'   => 3306, // 端口
 	'DB_PREFIX'=>'vvvle_' ,  //数据库表前缀
 );
